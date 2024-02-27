9050d8459c9eadbcc1be8422775d319d33d8e947
Zhuohan Lao <zlao@mit.edu>
2024-02-27 16:27:31 -0500

    web

--- sphinx_files/source/conf.py
+++ sphinx_files/source/conf.py
@@ -65,6 +65,9 @@ autoclass_content = 'both'
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_logo = "images/website_pic.png" 
+html_favicon = "images/logo.png"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
